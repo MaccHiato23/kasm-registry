bb829fcd3cdfbfa05eb8ec8fe397065ab94169b4
Devin Catuns <devincatuns1@gmail.com>
2026-06-27 17:47:57 -0400

    fix: update logo img url

--- site/next.config.js
+++ site/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   env: {
     name: 'MaccHiato23',
     description: 'The official store for MaccHiato23 supported workspaces.',
-    icon: 'https://macchiato23.github.io/kasm-registry/1.0/img/macchiato-tech-logo.png',
+    icon: 'https://macchiato23.github.io/kasm-registry/1.0/logo.png',
     listUrl: 'https://macchiato23.github.io/kasm-registry/',
     contactUrl: 'https://github.com/macchiato23/kasm-registry/issues',
   },
